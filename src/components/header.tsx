c0cb466e8b1e0febd7c281f7fa08bec14da0c373
black-adm <mattheuscontato17@gmail.com>
2023-10-30 02:13:13 -0300

    header component finished

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { Search, ShoppingCart } from "lucide-react";
 
 export function Header() {
     return (
@@ -16,14 +17,30 @@ export function Header() {
                         bugstore
                     </span>
                 </Link>
- 
-                <form className="bg-primary-white flex w-[320px] items-center gap-3 rounded-lg px-5 py-3.5 ring-light-purple">
 
+                <form className="ml-8 bg-primary-white flex w-[320px] items-center gap-3 rounded-full px-4 py-2 ring-light-purple">
+                    <Search className="h-5 w-5 text-medium-violet" />
+                    <input placeholder="Buscar produtos" className="flex-1 bg-primary-white text-sm text-dark-violet outline-none placeholder:text-dark-violet" />
                 </form>
             </div>
 
             <div className="flex items-center gap-4">
+                <div className="flex items-center gap-2">
+                    <ShoppingCart className="h-6 w-6 text-light-purple hover:text-medium-violet" />
+                    <span className="text-sm">(0)</span>
+                </div>
+                <div className="w-px h-4 bg-medium-violet" />
 
+                <Link href="/" className="flex items-center gap-2 hover:underline hover:text-medium-violet">
+                    <span className="text-sm">Conta</span>
+                    <Image
+                        src="https://github.com/black-adm.png"
+                        className="h-6 w-6 rounded-full"
+                        width="24"
+                        height="24"
+                        alt=""
+                    />
+                </Link>
             </div>
         </div>
     )
